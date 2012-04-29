9ffd289b03ba20b8b1264ac460185941b0c5acb7
Johannes Seitz <software@printhelloworld.de>
2012-04-29 14:08:47 +0200

    Rename to match other test classes
    Signed-off-by: Johannes Seitz <software@printhelloworld.de>

---
+++ src/test/java/payrollcasestudy/transactions/change/ChangeNameTransactionTest.java
@@ -0,0 +1,30 @@
+package payrollcasestudy.transactions.change;
+
+import org.junit.Rule;
+import org.junit.Test;
+import payrollcasestudy.entities.Employee;
+import payrollcasestudy.transactions.DatabaseResource;
+import payrollcasestudy.transactions.add.AddHourlyEmployeeTransaction;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class ChangeNameTransactionTest {
+
+    @Rule
+    public DatabaseResource databaseResource = new DatabaseResource();
+
+    @Test
+    public void testChangeNameTransaction() throws Exception {
+        int employeeId = 2;
+        AddHourlyEmployeeTransaction addEmployeeTransaction =
+                new AddHourlyEmployeeTransaction(employeeId, "Bill", "Home", 15.25);
+        addEmployeeTransaction.execute();
+
+        ChangeNameTransaction changeNameTransaction = new ChangeNameTransaction(employeeId, "Bob");
+        changeNameTransaction.execute();
+
+        Employee employee = databaseResource.getInstance().getEmployee(employeeId);
+        assertThat(employee.getName(), is("Bob"));
+    }
+}
